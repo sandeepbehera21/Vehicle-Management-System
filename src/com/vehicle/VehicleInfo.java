316601b17b90c528e3ab6bb55960399ea2d4fb27
Pratik Kumar Dash <Pratikdash9938@gmail.com>
2025-10-20 18:53:53 +0530

    Add getters and setters for vehicle properties

--- src/com/vehicle/VehicleInfo.java
+++ src/com/vehicle/VehicleInfo.java
@@ -9,3 +9,68 @@ public class VehicleInfo {
     private String image_url;
     private double rent_per_day;
     private boolean availability;
+
+    public int getVehicle_id() {
+        return vehicle_id;
+    }
+
+    public void setVehicle_id(int vehicle_id) {
+        this.vehicle_id = vehicle_id;
+    }
+
+    public int getOwner_id() {
+        return owner_id;
+    }
+
+    public void setOwner_id(int owner_id) {
+        this.owner_id = owner_id;
+    }
+
+    public String getVehicle_name() {
+        return vehicle_name;
+    }
+
+    public void setVehicle_name(String vehicle_name) {
+        this.vehicle_name = vehicle_name;
+    }
+
+    public String getVehicle_type() {
+        return vehicle_type;
+    }
+
+    public void setVehicle_type(String vehicle_type) {
+        this.vehicle_type = vehicle_type;
+    }
+
+    public String getVehicle_number() {
+        return vehicle_number;
+    }
+
+    public void setVehicle_number(String vehicle_number) {
+        this.vehicle_number = vehicle_number;
+    }
+
+    public String getImage_url() {
+        return image_url;
+    }
+
+    public void setImage_url(String image_url) {
+        this.image_url = image_url;
+    }
+
+    public double getRent_per_day() {
+        return rent_per_day;
+    }
+
+    public void setRent_per_day(double rent_per_day) {
+        this.rent_per_day = rent_per_day;
+    }
+
+    public boolean isAvailability() {
+        return availability;
+    }
+
+    public void setAvailability(boolean availability) {
+        this.availability = availability;
+    }
+}
