9703e16fe9c5360f0621f935abdaf156e4b9eec2
Pratik Kumar Dash <Pratikdash9938@gmail.com>
2025-10-20 18:36:30 +0530

    Add VehicleInfo class with vehicle properties

---
+++ src/com/vehicle/VehicleInfo.java
@@ -0,0 +1,11 @@
+package com.vehicle;
+
+public class VehicleInfo {
+    private int vehicle_id;
+    private int owner_id;
+    private String vehicle_name;
+    private String vehicle_type;
+    private String vehicle_number;
+    private String image_url;
+    private double rent_per_day;
+    private boolean availability;
