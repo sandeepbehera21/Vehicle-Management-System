642825f93d1fbdc944512653e413dbb9b685bbc9
BADAL SINGH <badalboss47@gmail.com>
2025-10-24 07:15:04 +0530

    Add MaintenanceData class for vehicle maintenance

---
+++ src/com/vehicle/MaintenanceData.java
@@ -0,0 +1,10 @@
+package com.vehicle;
+
+public class MaintenanceData {
+    public int maintenanceId;
+    public int vehicleId;
+    public String vehicleName;
+    public String maintenanceDate;
+    public String description;
+    public double cost;
+}
